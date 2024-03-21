53152c55f43a0fd01fe03e8664a5160fa15250b4
Larry Loop <larry.loop@devops.ch>
2024-03-21 20:50:21 +0100

    logarithm with base attribute

--- advanced.py
+++ advanced.py
@@ -3,7 +3,7 @@ import math
 def power(a, b):
     return a ** b
 
-def logarithm(a):
+def logarithm(a, base):
     if a <= 0:
         raise ValueError('Logarithm of x <= is not defined')
-    return math.log(a)
+    return math.log(a, base)
